4d667c5b2f94e6d0640cc8bd262c6e38b8e090e8
Katie Ní Ghilín <127398571+knighilin@users.noreply.github.com>
2023-03-09 11:01:37 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "Intro Music":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004520000000400012004000800012208000c00020f2410001400012014001800012218001c00020c2420002400012024002800012228002c00020f242c003000012930003400012734003800012538004000020824`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
